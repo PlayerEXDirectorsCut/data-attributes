c3c84c7f13b23088322de25bc4ccea67a9de64d4
Edelweiss <redwitblue@gmail.com>
2023-12-30 06:03:38 -0600

    Fixed Mixins for Equipments and Items

--- src/main/java/com/github/clevernucleus/dataattributes/mixin/item/ItemMixin.java
+++ src/main/java/com/github/clevernucleus/dataattributes/mixin/item/ItemMixin.java
@@ -5,16 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import com.github.clevernucleus.dataattributes.api.item.ItemHelper;
 
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.sound.SoundEvent;
 
 @Mixin(Item.class)
 abstract class ItemMixin implements ItemHelper {
-
-    // Overrides the getEquipSound method from ItemHelper
-    @Override
-    public SoundEvent getEquipSound(final ItemStack itemStack) {
-        // Calls the original getEquipSound method from Item
-        return ((Item) (Object) this).getEquipSound();
-    }
 }
