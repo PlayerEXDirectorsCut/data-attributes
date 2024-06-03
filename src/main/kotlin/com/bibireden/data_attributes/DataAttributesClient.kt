d52334ec6bf4fdbf2e2c9b726067570d21c96ac3
Bibi Reden <redwitblue@gmail.com>
2024-06-03 18:28:25 -0500

    Reimplement Client init, decouple network channels

---
+++ src/main/kotlin/com/bibireden/data_attributes/DataAttributesClient.kt
@@ -0,0 +1,34 @@
+package com.bibireden.data_attributes
+
+import com.bibireden.data_attributes.data.AttributeResourceManager
+import com.bibireden.data_attributes.networking.Channels
+import io.wispforest.endec.format.bytebuf.ByteBufDeserializer
+import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.networking.v1.ClientLoginNetworking
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs
+import net.minecraft.client.MinecraftClient
+import net.minecraft.network.PacketByteBuf
+import java.util.concurrent.CompletableFuture
+
+class DataAttributesClient : ClientModInitializer {
+    companion object {
+        fun onPacketRecieved(client: MinecraftClient, buf: PacketByteBuf) {
+            client.execute {
+                DataAttributes.CLIENT_MANAGER = AttributeResourceManager.ENDEC.decodeFully(ByteBufDeserializer::of, buf)
+                DataAttributes.CLIENT_MANAGER.onDataUpdate()
+            }
+        }
+    }
+
+    override fun onInitializeClient() {
+        ClientLoginNetworking.registerGlobalReceiver(Channels.HANDSHAKE) { client, _, buf, _ ->
+            onPacketRecieved(client, buf)
+            CompletableFuture.completedFuture(PacketByteBufs.empty())
+        }
+
+        ClientPlayNetworking.registerGlobalReceiver(Channels.RELOAD) { client, _, buf, sender ->
+            onPacketRecieved(client, buf)
+        }
+    }
+}
---
+++ src/main/kotlin/com/bibireden/data_attributes/networking/Channels.kt
@@ -0,0 +1,8 @@
+package com.bibireden.data_attributes.networking
+
+import com.bibireden.data_attributes.api.DataAttributesAPI
+
+object Channels {
+    val HANDSHAKE = DataAttributesAPI.id("handshake")
+    val RELOAD = DataAttributesAPI.id("reload")
+}
