f91675bbbbd6bc502b843211eb963b475f1656b2
Bibi Reden <redwitblue@gmail.com>
2024-08-12 17:22:02 -0500

    [fix] adjust formatting of `Whole` attributes

--- src/main/kotlin/com/bibireden/data_attributes/api/attribute/AttributeFormat.kt
+++ src/main/kotlin/com/bibireden/data_attributes/api/attribute/AttributeFormat.kt
@@ -3,8 +3,8 @@ package com.bibireden.data_attributes.api.attribute
 import io.wispforest.endec.Endec
 
 enum class AttributeFormat(val function: (min: Double, max: Double, value: Double) -> String) {
-    Percentage ({min, max, value -> "%.2f".format((value-min)/((max-min)/100))+"%" }),
-    Whole({_, _, value -> "$value"} );
+    Percentage ({min, max, value -> "%.2f".format((value - min) / ((max - min) / 100)) + "%" }),
+    Whole({_, _, value -> "%.2f".format(value) } );
 
     companion object {
         val ENDEC: Endec<AttributeFormat> = Endec.STRING.xmap(AttributeFormat::of) { it.name }
