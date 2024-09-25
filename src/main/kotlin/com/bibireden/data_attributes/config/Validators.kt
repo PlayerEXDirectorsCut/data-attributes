22311f4502b1f6cfea4afb02b38abfdada5bcbb3
Bibi Reden <redwitblue@gmail.com>
2024-09-25 12:46:27 -0500

    [chore] Make `Validators` internal

--- src/main/kotlin/com/bibireden/data_attributes/config/Validators.kt
+++ src/main/kotlin/com/bibireden/data_attributes/config/Validators.kt
@@ -1,5 +1,8 @@
 package com.bibireden.data_attributes.config
 
+import org.jetbrains.annotations.ApiStatus
+
+@ApiStatus.Internal
 object Validators {
     /** Checks if the [String] is a valid number via. Regex. */
     fun isNumeric(str: String) = str.matches("^[0-9.-]*\$".toRegex())
