4de275df32e4d2388f81a48e7bdb3c2dad83c457
Bibi Reden <redwitblue@gmail.com>
2025-01-27 20:20:17 -0600

    [feat] migrate fabric events/networking to own sector

--- common/src/main/kotlin/com/bms/data_attributes/DataAttributesClient.kt
+++ common/src/main/kotlin/com/bms/data_attributes/DataAttributesClient.kt
@@ -2,10 +2,9 @@ package com.bms.data_attributes
 
 import com.bms.data_attributes.config.AttributeConfigManager
 import com.bms.data_attributes.config.ConfigUIState
+import com.bms.data_attributes.config.impl.AttributeConfigManager
 import com.bms.data_attributes.ui.DataAttributesConfigScreen
 import io.wispforest.owo.config.ui.ConfigScreenProviders
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
-import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry
 
 object DataAttributesClient {
     @JvmField
@@ -15,18 +14,12 @@ object DataAttributesClient {
     val UI_STATE = ConfigUIState()
 
     /** Whenever the client receives a sync packet from the server to update the world-state via. configuration. */
-    private fun onPacketReceived(packet: AttributeConfigManager.Packet) {
+    fun onPacketReceived(packet: AttributeConfigManager.Packet) {
         MANAGER.readPacket(packet)
         MANAGER.onDataUpdate()
     }
 
     fun init() {
-        PayloadTypeRegistry.playC2S().register(AttributeConfigManager.Packet.PACKET_ID, AttributeConfigManager.Packet.PACKET_CODEC)
-
         ConfigScreenProviders.register(DataAttributes.MOD_ID) { DataAttributesConfigScreen(DataAttributes.OVERRIDES_CONFIG, DataAttributes.FUNCTIONS_CONFIG, DataAttributes.ENTITY_TYPES_CONFIG, it) }
-
-        ClientPlayNetworking.registerGlobalReceiver(AttributeConfigManager.Packet.PACKET_ID) { packet, _ ->
-            onPacketReceived(packet)
-        }
     }
 }
--- fabric/src/main/kotlin/com/bms/data_attributes/fabric/DataAttributesInitializerClientFabric.kt
+++ fabric/src/main/kotlin/com/bms/data_attributes/fabric/DataAttributesInitializerClientFabric.kt
@@ -1,11 +1,20 @@
 package com.bms.data_attributes.fabric
 
 import com.bms.data_attributes.DataAttributesClient
+import com.bms.data_attributes.config.impl.AttributeConfigManager
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
+import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry
 
 @Suppress("unused")
 class DataAttributesInitializerClientFabric : ClientModInitializer {
     override fun onInitializeClient() {
         DataAttributesClient.init()
+
+        PayloadTypeRegistry.playC2S().register(AttributeConfigManager.Packet.PACKET_ID, AttributeConfigManager.Packet.PACKET_CODEC)
+
+        ClientPlayNetworking.registerGlobalReceiver(AttributeConfigManager.Packet.PACKET_ID) { packet, _ ->
+            DataAttributesClient.onPacketReceived(packet)
+        }
     }
 }
